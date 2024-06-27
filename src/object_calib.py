39e0d48240eef3c981a4eed941d50bc3194d4e04
Gonçalo Frazão <gonfrazao@gmail.com>
2024-06-27 14:37:40 +0100

    vican files updated

--- src/object_calib.py
+++ src/object_calib.py
@@ -1,26 +1,27 @@
 import os
 import torch
 
-from src.vican.cam import estimate_pose_mp
-from src.vican.dataset import Dataset
-from src.vican.parse_config import parse_config
+from vican.cam import estimate_pose_mp
+from vican.dataset import Dataset
+from vican.parse_config import parse_config
 
-
-def object_calib(DATASET_PATH):
+def object_calib(DATASET_PATH='/dataset'):
     must_have_keys = ['object_path', 'object_calib', 'aruco', 'marker_size', 'marker_ids', 'brightness', 'contrast']
     config = parse_config(DATASET_PATH, must_have_keys)
 
     obj_dataset = Dataset(root=os.path.join(DATASET_PATH, config['object_path']))
-    print(config)
 
     aux = estimate_pose_mp(cams=obj_dataset.im_data['cam'],
                         im_filenames=obj_dataset.im_data['filename'],
                         aruco=config['aruco'],
                         marker_size=config['marker_size'],
-                        corner_refine='CORNER_REFINE_APRILTAG',
+                        corner_refine='CORNER_REFINE_SUBPIX',
                         marker_ids=config['marker_ids'],
                         flags='SOLVEPNP_IPPE_SQUARE',
                         brightness=config['brightness'],
                         contrast=config['contrast'])
 
     torch.save(aux, os.path.join(DATASET_PATH, config['object_calib']))
+
+if __name__ == "__main__":
+    object_calib()
--- src/pose_est.py
+++ src/pose_est.py
@@ -5,12 +5,12 @@ from shapely.geometry import Polygon
 import scipy.io as sio
 import json
 
-from src.vican.cam import estimate_pose_mp
-from src.vican.bipgo import bipartite_se3sync, object_bipartite_se3sync
-from src.vican.dataset import Dataset
-from src.vican.parse_config import parse_config
+from vican.cam import estimate_pose_mp
+from vican.bipgo import bipartite_se3sync, object_bipartite_se3sync
+from vican.dataset import Dataset
+from vican.parse_config import parse_config
 
-def pose_est(DATASET_PATH):
+def pose_est(DATASET_PATH='/dataset'):
     must_have_keys = ['object_calib', 'cameras_path', 'aruco', 'marker_size', 'marker_ids', 'brightness', 'contrast']
     config = parse_config(DATASET_PATH, must_have_keys)
 
@@ -19,8 +19,8 @@ def pose_est(DATASET_PATH):
     aux = torch.load(os.path.join(DATASET_PATH, config['object_calib']))
 
     obj_pose_est = object_bipartite_se3sync(aux,
-                                            noise_model_r=lambda edge : 0.01, #* Polygon(zip(edge['corners'][:,0], edge['corners'][:,1])).area**2,
-                                            noise_model_t=lambda edge : 0.01, #* Polygon(zip(edge['corners'][:,0], edge['corners'][:,1])).area**6,
+                                            noise_model_r=lambda edge : 0.01 * Polygon(zip(edge['corners'][:,0], edge['corners'][:,1])).area**1,
+                                            noise_model_t=lambda edge : 0.001 * Polygon(zip(edge['corners'][:,0], edge['corners'][:,1])).area**1,
                                             edge_filter=lambda edge : edge['reprojected_err'] < 0.5,
                                             maxiter=4,
                                             lsqr_solver="conjugate_gradient",
@@ -30,7 +30,7 @@ def pose_est(DATASET_PATH):
                                         im_filenames=dataset.im_data['filename'],
                                         aruco=config['aruco'],
                                         marker_size=config['marker_size'],
-                                        corner_refine=None,
+                                        corner_refine='CORNER_REFINE_SUBPIX',
                                         marker_ids=config['marker_ids'],
                                         flags='SOLVEPNP_IPPE_SQUARE',
                                         brightness=config['brightness'],
@@ -38,17 +38,17 @@ def pose_est(DATASET_PATH):
     
     # torch.save(cam_marker_edges, os.path.join(DATASET_PATH, 'room_pose.pt'))
 
-    tmax = 2000
-    edges = {k : v for k, v in cam_marker_edges.items() if int(k[1].split('_')[0]) < tmax}
+    # tmax = 2000
+    # edges = {k : v for k, v in cam_marker_edges.items() if int(k[1].split('_')[0]) < tmax}
 
-    pose_est = bipartite_se3sync(edges,
+    pose_est = bipartite_se3sync(cam_marker_edges,
                                 constraints=obj_pose_est,
                                 noise_model_r=lambda edge : 0.001 * Polygon(zip(edge['corners'][:,0], edge['corners'][:,1])).area**1.0,
-                                noise_model_t=lambda edge : 0.001 * Polygon(zip(edge['corners'][:,0], edge['corners'][:,1])).area**2.0,
+                                noise_model_t=lambda edge : 0.001 * Polygon(zip(edge['corners'][:,0], edge['corners'][:,1])).area**1.0,
                                 edge_filter=lambda edge : edge['reprojected_err'] < 0.5,
                                 maxiter=4,
                                 lsqr_solver="conjugate_gradient",
-                                dtype=np.float64)
+                                dtype=np.float32)
     
     json_data = {}
     for i in pose_est:
@@ -56,3 +56,6 @@ def pose_est(DATASET_PATH):
     
     with open(os.path.join(DATASET_PATH, config['cameras_pose_est']), 'w') as f:
         json.dump(json_data, f)
+
+if __name__ == "__main__":
+    pose_est()
